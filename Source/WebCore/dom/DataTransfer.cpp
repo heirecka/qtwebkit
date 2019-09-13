d8d7e961eab29ddc97c4fc605c81944f86fbaee5
Konstantin Tokarev <annulen@yandex.ru>
2019-09-13 22:13:45 +0300

    wip Use QImage instead of QImage in NativeImagePtr and DragImageRef
    QImage is internally a pointer to data with shared ownership, which makes
    its behavior close to RefPtr<cairo_surface_t> and RetainPtr<NSImage>.
    
    OTOH, this change a) breaks source compatibility with nullptr and boolean
    conversions; b) can lead to unexpected detaches and writing image changes
    to object's copy.
    
    For some reason, this change fixes existing issues with image decoding.
    
    TODO: Implement wrapper object for QImage, check ownership model, make
    sure that QPixmap::fromImage is called with QImage&& where appropriate
    
    Change-Id: I7e9f8e69f4fdcb335dd7b055b659b7d8b36a2e89

--- Source/WebCore/dom/DataTransfer.cpp
+++ Source/WebCore/dom/DataTransfer.cpp
@@ -551,7 +551,7 @@ DragImageRef DataTransfer::createDragImage(IntPoint& location) const
     }
 
     // We do not have enough information to create a drag image, use the default icon.
-    return nullptr;
+    return QImage();
 }
 
 #endif
